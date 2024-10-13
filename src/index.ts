f6e26b5ed2c55b6ca8a6f87a9ecc9f71ffae569c
DiegoMMF <diegomaldinifreyre@gmail.com>
2024-10-13 08:34:49 -0300

    streaming json output parser

--- src/index.ts
+++ src/index.ts
@@ -1,58 +1,97 @@
 import dotenv from "dotenv";
 import { model } from "./model/model";
-import { z } from "zod";
-import { RunnableSequence } from "@langchain/core/runnables";
-import { StructuredOutputParser } from "@langchain/core/output_parsers";
+import { JsonOutputParser } from "@langchain/core/output_parsers";
 import { ChatPromptTemplate } from "@langchain/core/prompts";
 
 dotenv.config();
 
-const zodSchema = z.object({
-  answer: z.string().describe("answer to the user's question"),
-  source: z
-    .string()
-    .describe("source used to answer the user's question, should be a website."),
-});
+const template = `Return a JSON object with a single key named "answer" that answers the following question: {question}.
+Do not wrap the JSON output in markdown blocks.`;
 
-const parser = StructuredOutputParser.fromZodSchema(zodSchema);
+const jsonPrompt = ChatPromptTemplate.fromTemplate(template);
+const jsonParser = new JsonOutputParser();
+const jsonChain = jsonPrompt.pipe(model).pipe(jsonParser);
 
-const chain = RunnableSequence.from([
-  ChatPromptTemplate.fromTemplate(
-    "Answer the users question as best as possible.\n{format_instructions}\n{question}"
-  ),
-  model,
-  parser,
-]);
-
-// Primero, imprimimos las instrucciones de formato
-console.log(parser.getFormatInstructions());
-
-/* const main = async () => {
-  const chain = RunnableSequence.from([promptTemplate, model, parser]);
-  const result = await chain.invoke({
-    language: "español",
-    input: "Hello, world!",
+const main = async () => {
+  const stream = await jsonChain.stream({
+    question: "Who invented the microscope?",
   });
-  console.log(result);
+
+  for await (const chunk of stream) console.log(chunk);
 };
 
-main().catch(console.error); */
+main().catch(console.error);
 
 /*
 Terminal output:
 
-You must format your output as a JSON value that adheres to a given "JSON Schema" instance.
-
-"JSON Schema" is a declarative language that allows you to annotate and validate JSON documents.
-
-For example, the example "JSON Schema" instance {{"properties": {{"foo": {{"description": "a list of test words", "type": "array", "items": {{"type": "string"}}}}}}, "required": ["foo"]}}}}
-would match an object with one required property, "foo". The "type" property specifies "foo" must be an "array", and the "description" property semantically describes it as "a list of test words". The items within "foo" must be strings.
-Thus, the object {{"foo": ["bar", "baz"]}} is a well-formatted instance of this example "JSON Schema". The object {{"properties": {{"foo": ["bar", "baz"]}}}} is not well-formatted.
-
-Your output will be parsed and type-checked according to the provided schema instance, so make sure all fields in your output match the schema exactly and there are no trailing commas!
-
-Here is the JSON Schema instance your output must adhere to. Include the enclosing markdown codeblock:
-```json
-{"type":"object","properties":{"answer":{"type":"string","description":"answer to the user's question"},"source":{"type":"string","description":"source used to answer the user's question, should be a website."}},"required":["answer","source"],"additionalProperties":false,"$schema":"http://json-schema.org/draft-07/schema#"}
-```
+{ answer: '' }
+{ answer: 'The' }
+{ answer: 'The invention' }
+{ answer: 'The invention of' }
+{ answer: 'The invention of the' }
+{ answer: 'The invention of the micro' }
+{ answer: 'The invention of the microscope' }
+{ answer: 'The invention of the microscope is' }
+{ answer: 'The invention of the microscope is attributed' }
+{ answer: 'The invention of the microscope is attributed to' }
+{ answer: 'The invention of the microscope is attributed to Zach' }
+{ answer: 'The invention of the microscope is attributed to Zachari' }
+{
+  answer: 'The invention of the microscope is attributed to Zacharias'
+}
+{
+  answer: 'The invention of the microscope is attributed to Zacharias J'
+}
+{
+  answer: 'The invention of the microscope is attributed to Zacharias Jans'
+}
+{
+  answer: 'The invention of the microscope is attributed to Zacharias Janssen'
+}
+{
+  answer: 'The invention of the microscope is attributed to Zacharias Janssen,'
+}
+{
+  answer: 'The invention of the microscope is attributed to Zacharias Janssen, a'
+}
+{
+  answer: 'The invention of the microscope is attributed to Zacharias Janssen, a Dutch'
+}
+{
+  answer: 'The invention of the microscope is attributed to Zacharias Janssen, a Dutch spect'
+}
+{
+  answer: 'The invention of the microscope is attributed to Zacharias Janssen, a Dutch spectacle'
+}
+{
+  answer: 'The invention of the microscope is attributed to Zacharias Janssen, a Dutch spectacle maker'
+}
+{
+  answer: 'The invention of the microscope is attributed to Zacharias Janssen, a Dutch spectacle maker,'
+}
+{
+  answer: 'The invention of the microscope is attributed to Zacharias Janssen, a Dutch spectacle maker, in'
+}
+{
+  answer: 'The invention of the microscope is attributed to Zacharias Janssen, a Dutch spectacle maker, in the'
+}
+{
+  answer: 'The invention of the microscope is attributed to Zacharias Janssen, a Dutch spectacle maker, in the late'
+}
+{
+  answer: 'The invention of the microscope is attributed to Zacharias Janssen, a Dutch spectacle maker, in the late 1'
+}
+{
+  answer: 'The invention of the microscope is attributed to Zacharias Janssen, a Dutch spectacle maker, in the late 16'
+}
+{
+  answer: 'The invention of the microscope is attributed to Zacharias Janssen, a Dutch spectacle maker, in the late 16th'
+}
+{
+  answer: 'The invention of the microscope is attributed to Zacharias Janssen, a Dutch spectacle maker, in the late 16th century'
+}
+{
+  answer: 'The invention of the microscope is attributed to Zacharias Janssen, a Dutch spectacle maker, in the late 16th century.'
+}
 */
