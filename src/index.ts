cf6595e06979b06be836c9d3861ee5ae3bdc4b5b
DiegoMMF <diegomaldinifreyre@gmail.com>
2024-10-13 10:09:07 -0300

    prompt templates and json parser

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,43 @@
 import dotenv from "dotenv";
-import { RunnableLambda, RunnableSequence } from "@langchain/core/runnables";
+import { model } from "./model/model";
+import { JsonOutputParser } from "@langchain/core/output_parsers";
+import { PromptTemplate } from "@langchain/core/prompts";
 
 dotenv.config();
 
-const firstLambda = new RunnableLambda({ func: (x: number) => x + 1 });
+const jokeQuery = "Dime un chiste";
 
-const secondLambda = new RunnableLambda({ func: (x: number) => x * 2 });
+const parser = new JsonOutputParser();
 
-const thirdLambda = new RunnableLambda({ func: (x: number) => x * 5 });
+const promptWithParser = new PromptTemplate({
+  template: `
+    Responde la solicitud del usuario: {query}
 
-const sequence = RunnableSequence.from([
-  firstLambda,
-  {
-    secondLambda,
-    thirdLambda,
-  },
-]);
+    Tu respuesta DEBE ser un objeto JSON válido con la siguiente estructura:
+    {{
+      "chiste": "El chiste aquí",
+      "explicacion": "Una breve explicación del chiste (opcional)"
+    }}
 
-const main = async () => console.log(await sequence.invoke(10));
+    {formatInstructions}
+  `,
+  inputVariables: ["query"],
+  partialVariables: { formatInstructions: parser.getFormatInstructions() },
+});
+
+const chain = promptWithParser.pipe(model).pipe(parser);
+
+const main = async () => {
+  const result = await chain.invoke({ query: jokeQuery });
+  console.log(result);
+};
 
 main().catch(console.error);
 
 /* Terminal output:
 
-{ secondLambda: 22, thirdLambda: 55 }
-
+{
+  chiste: '¿Por qué no se invita a Tetris a las fiestas? Porque siempre pone líneas.',
+  explicacion: 'Este chiste es gracioso porque hace una analogía entre el juego Tetris y las fiestas. En Tetris, el objetivo es encajar piezas geométricas llamadas tetrominós para crear líneas completas y eliminarlas. La broma está en que, si Tetris fuera una persona, siempre estaría creando líneas y posiblemente arruinaría la diversión en una fiesta, donde las líneas no son deseables.'
+}
 */
---
+++ src/index15.ts
@@ -0,0 +1,28 @@
+import dotenv from "dotenv";
+import { RunnableLambda, RunnableSequence } from "@langchain/core/runnables";
+
+dotenv.config();
+
+const firstLambda = new RunnableLambda({ func: (x: number) => x + 1 });
+
+const secondLambda = new RunnableLambda({ func: (x: number) => x * 2 });
+
+const thirdLambda = new RunnableLambda({ func: (x: number) => x * 5 });
+
+const sequence = RunnableSequence.from([
+  firstLambda,
+  {
+    secondLambda,
+    thirdLambda,
+  },
+]);
+
+const main = async () => console.log(await sequence.invoke(10));
+
+main().catch(console.error);
+
+/* Terminal output:
+
+{ secondLambda: 22, thirdLambda: 55 }
+
+*/
