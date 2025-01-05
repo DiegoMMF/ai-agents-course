1a88924153f1f1bb6f04fb5d72103ef1cfa60bd2
DiegoMMF <diegomaldinifreyre@gmail.com>
2025-01-05 14:38:35 -0300

    add 'ChatGoogleGenerativeAI' model

--- src/models.ts
+++ src/models.ts
@@ -1,9 +1,15 @@
-import { ChatGroq } from "@langchain/groq";
 import dotenv from "dotenv";
-
 dotenv.config();
+
+import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
+import { ChatGroq } from "@langchain/groq";
 
 export const chatGroq = new ChatGroq({
   model: "mixtral-8x7b-32768",
   temperature: 0,
+});
+
+export const chatGoogleGenerativeAI = new ChatGoogleGenerativeAI({
+  modelName: "gemini-1.5-flash",
+  maxOutputTokens: 2048,
 });
