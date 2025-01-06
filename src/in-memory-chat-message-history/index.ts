0e3b056f15f1ff6ed800a26f12f67b4e7ec6ea19
DiegoMMF <diegomaldinifreyre@gmail.com>
2025-01-06 20:16:07 -0300

    move index21 into /in-memory-chat-message-history

--- src/in-memory-chat-message-history/index.ts
+++ src/in-memory-chat-message-history/index.ts
@@ -1,17 +1,18 @@
 
-import { chatGroq } from "./models";
-import { msgs } from "./messages/messages";
+import { chatGroq } from "../models";
+import { msgs } from "../messages/messages";
 import {
   BaseChatMessageHistory,
   InMemoryChatMessageHistory,
 } from "@langchain/core/chat_history";
 import { RunnableWithMessageHistory } from "@langchain/core/runnables";
 import { HumanMessage, trimMessages } from "@langchain/core/messages";
-
+import { writeFileSync } from "fs";
 
 const chatHistory = new InMemoryChatMessageHistory(msgs.slice(0, -1));
 
-// The following is a dummy implementation of a function that gets the chat history for a given session ID
+// The following is a dummy implementation of a function that gets the chat history 
+// for a given session ID
 const dummyGetSessionHistory = (sessionId: string): BaseChatMessageHistory => {
   if (sessionId !== "1") {
     throw new Error("Session not found");
@@ -39,31 +40,7 @@ const main = async () => {
     [new HumanMessage("Hello, how are you?")],
     { configurable: { sessionId: "1" } }
   );
-  console.log(result);
+  writeFileSync(`${__dirname}/response.json`, JSON.stringify(result, null, 2));
 };
 
 main().catch(console.error);
-
-/* Terminal output:
-
-AIMessage {
-  "id": "run-0ddc0a55-e266-4d62-a718-fbe5b867ffe5",
-  "content": "Hello there! I'm just an AI, so I don't have feelings, but I'm here and ready to help you. How can I assist you today?",
-  "additional_kwargs": {},
-  "response_metadata": {
-    "tokenUsage": {
-      "completionTokens": 37,
-      "promptTokens": 122,
-      "totalTokens": 159
-    },
-    "finish_reason": "stop"
-  },
-  "tool_calls": [],
-  "invalid_tool_calls": [],
-  "usage_metadata": {
-    "input_tokens": 122,
-    "output_tokens": 37,
-    "total_tokens": 159
-  }
-}
-*/
