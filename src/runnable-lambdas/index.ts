6fabb5b6e59a4d4f8660282d404fd770a6f97cad
DiegoMMF <diegomaldinifreyre@gmail.com>
2025-01-06 10:27:13 -0300

    move index14 into /runnable-lambdas

--- src/runnable-lambdas/index.ts
+++ src/runnable-lambdas/index.ts
@@ -1,6 +1,5 @@
-
 import { RunnableLambda } from "@langchain/core/runnables";
-
+import { writeFileSync } from "fs";
 
 const firstLambda = new RunnableLambda({ func: (x: number) => x + 1 });
 
@@ -8,8 +7,13 @@ const secondLambda = new RunnableLambda({ func: (x: number) => x * 2 });
 
 const sequence = firstLambda.pipe(secondLambda);
 
-const main = async () => console.log(await sequence.invoke(10));
+const main = async () =>
+  writeFileSync(
+    `${__dirname}/response.txt`,
+    "La salida de la cadena es: " +
+      JSON.stringify(await sequence.invoke(10), null, 2) +
+      "\n",
+    { flag: "a" }
+  );
 
 main().catch(console.error);
-
-// Terminal output: 22
