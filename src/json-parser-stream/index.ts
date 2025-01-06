96dbc7d140833834d9465e7fbff85fed96cf1c87
DiegoMMF <diegomaldinifreyre@gmail.com>
2025-01-06 09:20:23 -0300

    move index11 into /json-parser-stream

---
+++ src/json-parser-stream/index.ts
@@ -0,0 +1,28 @@
+import { chatGroq } from "../models";
+import { JsonOutputParser } from "@langchain/core/output_parsers";
+import { ChatPromptTemplate } from "@langchain/core/prompts";
+import { writeFileSync } from "fs";
+
+const template = `Return a JSON object with a single key named "answer" that answers 
+the following question: {question}. Do not wrap the JSON output in markdown blocks.`;
+
+const jsonPrompt = ChatPromptTemplate.fromTemplate(template);
+const jsonParser = new JsonOutputParser();
+const jsonChain = jsonPrompt.pipe(chatGroq).pipe(jsonParser);
+
+const main = async () => {
+  const stream = await jsonChain.stream({
+    question: "Who invented the microscope?",
+  });
+
+  for await (const chunk of stream) {
+    // Write the output to a response.json file after newline without overwriting it.
+    writeFileSync(
+      `${__dirname}/response.json`,
+      JSON.stringify(chunk, null, 2) + "\n",
+      { flag: "a" }
+    );
+  }
+};
+
+main().catch(console.error);
