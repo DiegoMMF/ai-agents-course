8a18fe6d1fd5ecfcf94a938ab04eda08ce420a7f
DiegoMMF <diegomaldinifreyre@gmail.com>
2025-01-06 20:49:07 -0300

    merge index22 and index3 with /in-memory-chat-message-history

--- src/in-memory-chat-message-history/index.ts
+++ src/in-memory-chat-message-history/index.ts
@@ -1,9 +1,12 @@
 import { chatGroq } from "../models";
 import { RunnableWithMessageHistory } from "@langchain/core/runnables";
-import { HumanMessage } from "@langchain/core/messages";
-import { writeFileSync } from "fs";
-import { dummyGetSessionHistoryV1 as getMessageHistory } from "../messages/inMemoryChatMessageHistory";
+import { AIMessage, HumanMessage } from "@langchain/core/messages";
 import { trimmer } from "../messages/trimOptions";
+import {
+  dummyGetSessionHistoryV2 as getMessageHistory,
+  store,
+} from "../messages/inMemoryChatMessageHistory";
+import { writeFileSync } from "fs";
 
 const runnable = trimmer.pipe(chatGroq);
 
@@ -12,12 +15,48 @@ const chainWithHistory = new RunnableWithMessageHistory({
   getMessageHistory,
 });
 
+// Continuar con lo de abajo...
+
+// const promptTemplate = ChatPromptTemplate.fromMessages([
+//   new SystemMessage("You're a good assistant."),
+//   new MessagesPlaceholder("history"),
+//   new HumanMessage("What is my name?"),
+// ]);
+
 const main = async () => {
-  const result = await chainWithHistory.invoke(
+  // Case: No chat history
+  const loneResult = await chainWithHistory.invoke(
     [new HumanMessage("Hello, how are you?")],
     { configurable: { sessionId: "1" } }
   );
-  writeFileSync(`${__dirname}/response.json`, JSON.stringify(result, null, 2));
+  writeFileSync(`${__dirname}/response.json`, JSON.stringify(loneResult, null, 2));
+
+  // Case: Chat history exists
+  const sessionId = "myCurrentSession";
+  const chatHistory = getMessageHistory(sessionId);
+
+  await chatHistory.addMessages([
+    new HumanMessage("Hello, how are you? My name is Diego"),
+    new AIMessage("Hi. I'm here and ready to help you. How can I assist you today?"),
+  ]);
+
+  const result = await chainWithHistory.invoke(
+    [new HumanMessage("What is my name?")],
+    { configurable: { sessionId } }
+  );
+
+  const result2 = await chainWithHistory.invoke(
+    [new HumanMessage("What is my name?")],
+    { configurable: { sessionId: "anotherSession" } }
+  );
+
+  const responses = {
+    firstResponse: result.content,
+    firstChatHistory: store[sessionId],
+    secondResponse: result2.content,
+    secondChatHistory: store["anotherSession"],
+  };
+  writeFileSync(`${__dirname}/responses.json`, JSON.stringify(responses, null, 2));
 };
 
 main().catch(console.error);
