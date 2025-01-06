288a933d2058508289f5b22a55fa248bd306a8af
DiegoMMF <diegomaldinifreyre@gmail.com>
2025-01-06 10:51:36 -0300

    merge index16 into /json-parser-stream

--- src/json-parser-stream/index.ts
+++ src/json-parser-stream/index.ts
@@ -1,14 +1,33 @@
 import { chatGroq } from "../models";
 import { JsonOutputParser } from "@langchain/core/output_parsers";
-import { ChatPromptTemplate } from "@langchain/core/prompts";
+import { ChatPromptTemplate, PromptTemplate } from "@langchain/core/prompts";
 import { writeFileSync } from "fs";
 
-const template = `Return a JSON object with a single key named "answer" that answers 
+const parser = new JsonOutputParser();
+
+const firstTemplate = `Return a JSON object with a single key named "answer" that answers 
 the following question: {question}. Do not wrap the JSON output in markdown blocks.`;
 
-const jsonPrompt = ChatPromptTemplate.fromTemplate(template);
-const jsonParser = new JsonOutputParser();
-const jsonChain = jsonPrompt.pipe(chatGroq).pipe(jsonParser);
+const jsonPrompt = ChatPromptTemplate.fromTemplate(firstTemplate);
+const jsonChain = jsonPrompt.pipe(chatGroq).pipe(parser);
+
+const jokeQuery = "Dime un chiste";
+const promptWithParser = new PromptTemplate({
+  template: `
+    Responde la solicitud del usuario: {query}
+
+    Tu respuesta DEBE ser un objeto JSON válido con la siguiente estructura:
+    {{
+      "chiste": "El chiste aquí",
+      "explicacion": "Una breve explicación del chiste (opcional)"
+    }}
+
+    {formatInstructions}
+  `,
+  inputVariables: ["query"],
+  partialVariables: { formatInstructions: parser.getFormatInstructions() },
+});
+const pipedChain = promptWithParser.pipe(chatGroq).pipe(parser);
 
 const main = async () => {
   const stream = await jsonChain.stream({
@@ -23,6 +42,14 @@ const main = async () => {
       { flag: "a" }
     );
   }
+
+  const result = await pipedChain.invoke({ query: jokeQuery });
+
+  writeFileSync(
+    `${__dirname}/response.json`,
+    JSON.stringify(result, null, 2) + "\n",
+    { flag: "a" }
+  );
 };
 
 main().catch(console.error);
