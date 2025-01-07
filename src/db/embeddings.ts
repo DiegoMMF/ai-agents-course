47d8a98613250d5bcf6710e9b5db272d7df817a3
DiegoMMF <diegomaldinifreyre@gmail.com>
2025-01-07 20:18:40 -0300

    move index34 into /pinecone-embeddings

--- src/db/embeddings.ts
+++ src/db/embeddings.ts
@@ -4,6 +4,7 @@ dotenv.config();
 import type { Document } from "@langchain/core/documents";
 import { FireworksEmbeddings } from "@langchain/community/embeddings/fireworks";
 import { HuggingFaceInferenceEmbeddings } from "@langchain/community/embeddings/hf";
+import { Pinecone } from "@pinecone-database/pinecone";
 
 export const fireworksEmbeddings = new FireworksEmbeddings({
   modelName: "nomic-ai/nomic-embed-text-v1.5",
@@ -12,6 +13,8 @@ export const fireworksEmbeddings = new FireworksEmbeddings({
 export const hfEmbeddings = new HuggingFaceInferenceEmbeddings({
   apiKey: process.env.HUGGINGFACEHUB_API_KEY,
 });
+
+export const pc = new Pinecone({ apiKey: process.env.PINECONE_API_KEY as string });
 
 // * Mocked Documents for testing
 
@@ -44,3 +47,24 @@ const document4: Document = {
 };
 
 export const documentsB = [document1, document2, document3, document4];
+
+export const documentsC = [
+  {
+    id: "vec1",
+    text: "Apple is a popular fruit known for its sweetness and crisp texture.",
+  },
+  {
+    id: "vec2",
+    text: "The tech company Apple is known for its innovative products like the iPhone.",
+  },
+  { id: "vec3", text: "Many people enjoy eating apples as a healthy snack." },
+  {
+    id: "vec4",
+    text: "Apple Inc. has revolutionized the tech industry with its sleek designs and user-friendly interfaces.",
+  },
+  { id: "vec5", text: "An apple a day keeps the doctor away, as the saying goes." },
+  {
+    id: "vec6",
+    text: "Apple Computer Company was founded on April 1, 1976, by Steve Jobs, Steve Wozniak, and Ronald Wayne as a partnership.",
+  },
+];
--- src/pinecone-embeddings/index.ts
+++ src/pinecone-embeddings/index.ts
@@ -1,41 +1,16 @@
-// Import the Pinecone library
-import { Pinecone } from "@pinecone-database/pinecone";
-
-// Initialize a Pinecone client with your API key
-const pc = new Pinecone({ apiKey: process.env.PINECONE_API_KEY as string });
-
-// Define a sample dataset where each item has a unique ID and piece of text
-const data = [
-  {
-    id: "vec1",
-    text: "Apple is a popular fruit known for its sweetness and crisp texture.",
-  },
-  {
-    id: "vec2",
-    text: "The tech company Apple is known for its innovative products like the iPhone.",
-  },
-  { id: "vec3", text: "Many people enjoy eating apples as a healthy snack." },
-  {
-    id: "vec4",
-    text: "Apple Inc. has revolutionized the tech industry with its sleek designs and user-friendly interfaces.",
-  },
-  { id: "vec5", text: "An apple a day keeps the doctor away, as the saying goes." },
-  {
-    id: "vec6",
-    text: "Apple Computer Company was founded on April 1, 1976, by Steve Jobs, Steve Wozniak, and Ronald Wayne as a partnership.",
-  },
-];
+import { writeFileSync } from "fs";
+import { documentsC, pc } from "../db/embeddings";
 
 // Convert the text into numerical vectors that Pinecone can index
 const model = "multilingual-e5-large";
 
 // Create a serverless index
-const indexName = "example-index";
+const indexName = "example-index-3";
 
-(async () => {
+const main = async () => {
   const embeddings = await pc.inference.embed(
     model,
-    data.map((d) => d.text),
+    documentsC.map((d) => d.text),
     { inputType: "passage", truncate: "END" }
   );
 
@@ -51,8 +26,14 @@ const indexName = "example-index";
     },
   });
 
-  console.log({ embeddings, index });
-})();
+  writeFileSync(
+    `${__dirname}/responseEmbeddings.json`,
+    JSON.stringify(embeddings, null, 2)
+  );
+  writeFileSync(`${__dirname}/responseIndex.json`, JSON.stringify(index, null, 2));
+};
+
+main().catch(console.error);
 
 /* Terminal output:
 
