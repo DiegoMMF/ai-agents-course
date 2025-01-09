4dd0bace35373606de75959d2b6a205e90130da2
DiegoMMF <diegomaldinifreyre@gmail.com>
2025-01-09 13:33:30 -0300

    move consts and instantiations to 'config.ts'

---
+++ src/rag/config.ts
@@ -0,0 +1,64 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf";
+import { DirectoryLoader } from "langchain/document_loaders/fs/directory";
+import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
+import { HuggingFaceInferenceEmbeddings } from "@langchain/community/embeddings/hf";
+import { Chroma } from "@langchain/community/vectorstores/chroma";
+import { ChatPromptTemplate, MessagesPlaceholder } from "@langchain/core/prompts";
+
+// Carga de documentos PDF a partir de un directorio.
+export const directoryLoader = new DirectoryLoader("./src/rag/pdfs", {
+  ".pdf": (path) => new PDFLoader(path),
+});
+
+// Instanciamos y configuramos el splitter de documentos.
+export const textSplitter = new RecursiveCharacterTextSplitter({
+  chunkSize: 100,
+  chunkOverlap: 20,
+});
+
+// Instanciamos el modelo de embeddings de Hugging Face.
+export const hfEmbeddings = new HuggingFaceInferenceEmbeddings({
+  apiKey: process.env.HUGGINGFACEHUB_API_KEY,
+});
+
+// Instanciamos el vector store de Chroma.
+export const vectorStore = new Chroma(hfEmbeddings, {
+  collectionName: "a-test-collection", // collection name
+  url: "http://0.0.0.0:8000", // Chroma URL
+});
+
+// System prompt: se utiliza para que el modelo entienda el contexto de los
+// documentos que se le proporcionan.
+export const systemPrompt = `
+    Eres un asistente solícito que devuelve información relevante sobre los documentos
+    que se te proporcionan. Incluye emojis en tus respuestas para que sean más
+    amigables y fáciles de entender.
+    Tienes el siguiente contexto: {context}
+  `;
+
+// A diferencia del system prompt, este se utiliza para que el modelo entienda
+// el historial de conversación y el contexto que se le proporciona.
+export const contextualizedQSystemPrompt = `
+    Responde según el historial de conversación y el contexto que se te proporciona.
+    Si no tienes información relevante, simplemente responde con "No sé".
+    Además, responde de manera profesional a la pregunta que se te hace.
+  `;
+
+// Contextualized prompt
+// Adiferencia del prompt anterior, este se utiliza para que el modelo entienda
+// el historial de conversación y el contexto que se le proporciona.
+export const contextualizedQPrompt = ChatPromptTemplate.fromMessages([
+  ["system", contextualizedQSystemPrompt],
+  new MessagesPlaceholder("chat_history"),
+  ["human", "{input}"],
+]);
+
+// Prompt para la pregunta y respuesta
+export const qaPrompt = ChatPromptTemplate.fromMessages([
+  ["system", systemPrompt],
+  new MessagesPlaceholder("chat_history"),
+  ["human", "{input}"],
+]);
--- src/rag/index.ts
+++ src/rag/index.ts
@@ -1,104 +1,63 @@
-import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf";
-import { DirectoryLoader } from "langchain/document_loaders/fs/directory";
-import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
 import { getMessageHistory, saveOutput } from "./utils";
-import { chromaVectorStore } from "../db/embeddings";
 import { chatGroq as llm } from "../models/models";
-import { HumanMessage } from "@langchain/core/messages";
-import { ChatPromptTemplate, MessagesPlaceholder } from "@langchain/core/prompts";
-import { SystemMessage } from "@langchain/core/messages";
 import { createHistoryAwareRetriever } from "langchain/chains/history_aware_retriever";
 import { createStuffDocumentsChain } from "langchain/chains/combine_documents";
 import { createRetrievalChain } from "langchain/chains/retrieval";
 import { RunnableWithMessageHistory } from "@langchain/core/runnables";
+import {
+  contextualizedQPrompt,
+  directoryLoader,
+  qaPrompt,
+  textSplitter,
+  vectorStore,
+} from "./config";
 
 const main = async () => {
-  // Carga de documentos PDF a partir de un directorio
-  const loader = new DirectoryLoader("./src/rag/pdfs", {
-    ".pdf": (path) => new PDFLoader(path),
-  });
-
-  // Nota:
-  // Es importante saber que cuando los documentos son más largos, debemos separarlos
-  // en chunks. De esta manera, el modelo puede procesarlos de manera más eficiente.
-
-  // Splitting documents.
-  // El chunkSize y chunkOverlap deben ser ajustados según el tamaño de los docs.
-  // Si son muy largos, se debe aumentar el chunkSize y disminuir el chunkOverlap.
-  // Si son muy cortos, se debe disminuir el chunkSize y aumentar el chunkOverlap.
-  const textSplitter = new RecursiveCharacterTextSplitter({
-    chunkSize: 100,
-    chunkOverlap: 20,
-  });
-
-  const docs = await loader.load();
+  // Carga los documentos fuente en un array de documentos.
+  const docs = await directoryLoader.load();
   saveOutput("docs", docs);
 
+  // Divide los documentos en chunks de texto para procesarlos más eficientemente.
   const splittedDocs = await textSplitter.splitDocuments(docs);
   saveOutput("splittedDocs", splittedDocs);
 
-  // Adding documents to the vector store (LLM already added in definition)
+  // Traemos la Vector Store a la que previamente le agregamos el LLM de embeddings y
+  // le agregamos los chunks de texto.
+  const addedDocs = await vectorStore.addDocuments(splittedDocs);
   // Guardar addedDocs es innecesario pero se hace para que poder ver el resultado.
-  const addedDocs = await chromaVectorStore.addDocuments(splittedDocs);
   saveOutput("addedDocs", addedDocs);
 
-  // El retriever se encarga de buscar la información relevante en la vector store.
-  const retriever = chromaVectorStore.asRetriever();
+  // Instanciamos un VectorStoreRetriver cuyas queries no se basan en el contenido
+  // del texto sino en el embedding.
+  const retriever = vectorStore.asRetriever();
   saveOutput("retriever", retriever);
 
-  // System prompt
-  // Este prompt se utiliza para que el modelo entienda el contexto de los documentos
-  // que se le proporcionan.
-  const systemPrompt = `
-    Eres un asistente solícito que devuelve información relevante sobre los documentos
-    que se te proporcionan. Incluye emojis en tus respuestas para que sean más
-    amigables y fáciles de entender.
-    Tienes el siguiente contexto: {context}
-  `;
-
-  // Contextualized system prompt
-  // A diferencia del system prompt, este se utiliza para que el modelo entienda
-  // el historial de conversación y el contexto que se le proporciona.
-  const contextualizedQSystemPrompt = `
-    Responde según el historial de conversación y el contexto que se te proporciona.
-    Si no tienes información relevante, simplemente responde con "No sé".
-    Además, responde de manera profesional a la pregunta que se te hace.
-  `;
-
-  const contextualizedQPrompt = ChatPromptTemplate.fromMessages([
-    ["system", contextualizedQSystemPrompt],
-    new MessagesPlaceholder("chat_history"),
-    ["human", "{input}"],
-  ]);
-
-  // First, create the history-aware retriever
+  // Este retriever se encarga de buscar la información relevante en la vector store
+  // teniendo en cuenta el historial de conversación.
   const historyAwareRetriever = await createHistoryAwareRetriever({
     retriever,
     llm,
     rephrasePrompt: contextualizedQPrompt,
   });
+  saveOutput("historyAwareRetriever", historyAwareRetriever);
 
-  const qaPrompt = ChatPromptTemplate.fromMessages([
-    ["system", systemPrompt],
-    new MessagesPlaceholder("chat_history"),
-    ["human", "{input}"],
-  ]);
-
-  // Stuff Documents Chain
   // Combina los fragmentos recuperados con la pregunta del usuario para generar una
   // respuesta más completa y precisa.
-  const questionAnswerChain = createStuffDocumentsChain({ llm, prompt: qaPrompt });
+  const questionAnswerChain = await createStuffDocumentsChain({ llm, prompt: qaPrompt });
+  saveOutput("questionAnswerChain", questionAnswerChain);
 
-  // Retrieval Chain
-  // Generación de la Respuesta: El modelo de lenguaje toma los fragmentos recuperados
-  // y los utiliza como contexto adicional para generar la respuesta. Esta combinación
-  // de información permite que las respuestas estén basadas en información más reciente
-  // o específica, extraída de documentos cargados en el vector store.
+  // El LLM toma los fragmentos recuperados y los utiliza como contexto adicional
+  // para generar la respuesta. Esta combinación de información permite que las
+  // respuestas estén basadas en información más reciente o específica, extraída de
+  // documentos cargados en el vector store.
   const ragChain = await createRetrievalChain({
     retriever: historyAwareRetriever,
-    combineDocsChain: await questionAnswerChain,
+    combineDocsChain: questionAnswerChain,
   });
+  saveOutput("ragChain", ragChain);
 
+  // Wrapper que se encarga de agregar el historial de conversación al LLM y que
+  // el LLM pueda responder a la pregunta del usuario.
   const conversationalRagChain = new RunnableWithMessageHistory({
     runnable: ragChain,
     getMessageHistory,
@@ -106,6 +65,8 @@ const main = async () => {
     historyMessagesKey: "chat_history",
     outputMessagesKey: "answer",
   });
+  saveOutput("conversationalRagChain", conversationalRagChain);
+
 
   const queryResult = await conversationalRagChain.invoke(
     { input: "Cuáles son las ventas del último trimestre?" },
@@ -118,6 +79,12 @@ const main = async () => {
     { configurable: { sessionId: "user-123" } }
   );
   saveOutput("queryResult2", queryResult2);
+
+  const queryResult3 = await ragChain.invoke(
+    { input: "Qué puedo hacer en Mendoza?" },
+    { configurable: { sessionId: "user-123" } }
+  );
+  saveOutput("queryResult3", queryResult3);
 };
 
 main().catch(console.error);
